4fec1b462dafabbdfd93d190c72e5039b7136a88
monza001 <19781117@qq.com>
2014-10-18 23:50:47 +0800

    commit

--- LoveFinder/ViewController.swift
+++ LoveFinder/ViewController.swift
@@ -8,17 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var sex: UISegmentedControl!
+    
+    @IBOutlet weak var birthday: UIDatePicker!
+    
+    @IBOutlet weak var height: UISlider!
+    
+    @IBOutlet weak var heightLable: UILabel!
+    
+    @IBOutlet weak var house: UISwitch!
+    
+    @IBOutlet weak var result: UITextView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        name.delegate = self  //
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+
+    
+    @IBAction func heightChanged(sender: AnyObject) {
+        var slider = sender as UISlider
+        var i = Int(slider.value)
+        slider.value = Float(i)
+        
+        heightLable.text = "\(Int(slider.value))"
+    }
+    
+    
+    @IBAction func okDown(sender: AnyObject) {
+    }
+    
+    //UITextFieldDelegate 委托
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
 
 
 }
